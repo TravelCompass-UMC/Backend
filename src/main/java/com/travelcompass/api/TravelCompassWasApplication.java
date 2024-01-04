9746684ef80440e2452855a4372c7ec00c175ce2
enble <enble_777@naver.com>
2024-01-04 21:07:10 +0900

    Feat: 프로젝트 초기화

---
+++ src/main/java/com/travelcompass/api/TravelCompassWasApplication.java
@@ -0,0 +1,13 @@
+package com.travelcompass.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelCompassWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelCompassWasApplication.class, args);
+    }
+
+}
