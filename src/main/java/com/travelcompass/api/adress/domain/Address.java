be1fd24518a3cab3a336541d0586535e907bc656
HyunWoo <kimjd766@gmail.com>
2024-01-09 11:01:36 +0900

    Refactor: 행정동코드 타입 변경

--- src/main/java/com/travelcompass/api/adress/domain/Address.java
+++ src/main/java/com/travelcompass/api/adress/domain/Address.java
@@ -13,7 +13,7 @@ public class Address {
 
     @Id
     @Column(name = "ADDRESS_CODE")
-    private String addressCode;
+    private Integer addressCode;
 
     @Column(name = "SD_CODE")
     private String sidoCode;
@@ -27,7 +27,7 @@ public class Address {
     @Column(name = "RI_CODE")
     private String riCode;
 
-    public Address(String addressCode, String sidoCode, String sigunguCode, String umdCode, String riCode) {
+    public Address(Integer addressCode, String sidoCode, String sigunguCode, String umdCode, String riCode) {
         this.addressCode = addressCode;
         this.sidoCode = sidoCode;
         this.sigunguCode = sigunguCode;
