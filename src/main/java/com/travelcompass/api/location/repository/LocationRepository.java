6520411f97a1022efc43e5f4826a87f887a25b0f
enble <enble_777@naver.com>
2024-01-12 23:03:51 +0900

    Feat: LocationRepository 생성

--- src/main/java/com/travelcompass/api/location/repository/LocationRepository.java
+++ src/main/java/com/travelcompass/api/location/repository/LocationRepository.java
@@ -1,8 +1,10 @@
 package com.travelcompass.api.location.repository;
 
 import com.travelcompass.api.location.domain.Location;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
+    Optional<Location> findByName(String name);
 }
