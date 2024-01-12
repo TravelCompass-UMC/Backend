0bf6ad94bb7e67c67d603549e1e1ff1c9a34f1fc
enble <enble_777@naver.com>
2024-01-12 22:29:56 +0900

    Feat: Region 엔티티 생성

---
+++ src/main/java/com/travelcompass/api/region/domain/Region.java
@@ -0,0 +1,28 @@
+package com.travelcompass.api.region.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Region(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
