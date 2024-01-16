ae4ddaf397d546bb4a9c50c01e564b09446e454a
enble <enble_777@naver.com>
2024-01-16 16:29:16 +0900

    Feat: 영업시간 엔티티 생성

---
+++ src/main/java/com/travelcompass/api/location/domain/OpenHours.java
@@ -0,0 +1,41 @@
+package com.travelcompass.api.location.domain;
+
+import com.travelcompass.api.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OpenHours extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Week week;
+
+    private LocalTime openTime;
+
+    private LocalTime closeTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+}
