8d90c6181a861a66f24cf36d84c26ccb590103a0
persi0815 <jina_8151010@naver.com>
2024-01-18 15:29:51 +0900

    Refactor: 토큰 주소값으로 던지기

--- src/main/java/com/travelcompass/api/global/controller/TokenController.java
+++ src/main/java/com/travelcompass/api/global/controller/TokenController.java
@@ -1,57 +1,23 @@
 package com.travelcompass.api.global.controller;
 
-import com.travelcompass.api.oauth.jwt.JwtRequestDto;
-import com.travelcompass.api.oauth.jwt.JwtTokenDto;
 import com.travelcompass.api.oauth.jwt.JwtTokenUtils;
 import io.jsonwebtoken.Claims;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-@Slf4j
+// 토큰 확인 - 주소값으로 던지기
 @RestController
 @RequestMapping("token")
 public class TokenController {
-    private final UserDetailsManager manager;
-    private final PasswordEncoder passwordEncoder;
     private final JwtTokenUtils jwtTokenUtils;
 
     public TokenController(
-            UserDetailsManager manager,
-            PasswordEncoder passwordEncoder,
             JwtTokenUtils jwtTokenUtils
     ) {
-        this.manager = manager;
-        this.passwordEncoder = passwordEncoder;
         this.jwtTokenUtils = jwtTokenUtils;
-    }
-
-    @PostMapping("/issue") //토큰 생성
-    public JwtTokenDto issueJwt(@RequestBody JwtRequestDto dto) {
-        UserDetails userDetails
-                = manager.loadUserByUsername(dto.getUsername());
-
-        log.info(userDetails.getAuthorities().toString());
-        if (!passwordEncoder.matches(dto.getPassword(), userDetails.getPassword()))
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-
-        JwtTokenDto response = new JwtTokenDto();
-        response.setToken(jwtTokenUtils.generateToken(userDetails));
-        return response;
     }
 
     @GetMapping("/val")
     public Claims val(@RequestParam("token") String jwt) {
         return jwtTokenUtils.parseClaims(jwt);
     }
-
-    /*@GetMapping("/val") //토큰값 화면에 띄우기
-    public String val(@RequestParam("token") String jwt){
-        return "val";
-    }   */
 }
-
