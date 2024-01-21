acf9810ee30d81bfa338897ec5a2acfb8c8c1377
persi0815 <jina_8151010@naver.com>
2024-01-21 20:50:01 +0900

    Feature: HashTag

---
+++ src/main/java/com/travelcompass/api/hashtag/domain/HashtagPlan.java
@@ -0,0 +1,29 @@
+package com.travelcompass.api.hashtag.domain;
+
+import com.travelcompass.api.global.entity.BaseEntity;
+import com.travelcompass.api.plan.domain.Plan;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HashtagPlan extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hashtag_id")
+    private Hashtag hashtag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
+}
+
