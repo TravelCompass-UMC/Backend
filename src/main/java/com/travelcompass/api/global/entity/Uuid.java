f465c8335f038907eb12c83925de80924d4b5cca
enble <enble_777@naver.com>
2024-01-22 17:35:42 +0900

    Feat: Uuid 엔티티 추가

---
+++ src/main/java/com/travelcompass/api/global/entity/Uuid.java
@@ -0,0 +1,32 @@
+package com.travelcompass.api.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Uuid extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uuid;
+
+    private Uuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public static Uuid generateUuid() {
+        return new Uuid(UUID.randomUUID().toString());
+    }
+}
