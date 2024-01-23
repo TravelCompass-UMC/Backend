bab90d56304bf55c7561c29839dee0ae3b7f892c
HyunWoo <kimjd766@gmail.com>
2024-01-24 03:47:30 +0900

    feature: 여행계획의 상세정보 + 해쉬태그를 담을 수 있는 응답 DTO 추가

--- src/main/java/com/travelcompass/api/plan/dto/PlanResponseDto.java
+++ src/main/java/com/travelcompass/api/plan/dto/PlanResponseDto.java
@@ -1,6 +1,16 @@
 package com.travelcompass.api.plan.dto;
 
+import com.travelcompass.api.global.api_payload.ErrorCode;
+import com.travelcompass.api.global.exception.GeneralException;
+import com.travelcompass.api.plan.domain.Plan;
+import com.travelcompass.api.plan.domain.PlanLocation;
+import com.travelcompass.api.plan.domain.PlanVehicle;
+import com.travelcompass.api.region.domain.Region;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 @NoArgsConstructor
@@ -14,5 +24,21 @@ public class PlanResponseDto {
         Long planId;
 
         // 생성된 초대코드를 결과에 함께 넣어줘야 할듯
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DetailPlanResponseDto{
+        private Long id;
+        private String title;
+        private String startDate;
+        private String endDate;
+        private String inviteCode; // 초대를 위한 유니크한 코드
+        private String vehicle;
+        private String region;
+
+        private List<String> hashtag;
     }
 }
