ab30a6356116e455d4d0bf001e71ba292163002a
HyunWoo <kimjd766@gmail.com>
2024-01-23 21:53:57 +0900

    feature:Hashtag 관련 레포지터리 추가

--- src/main/java/com/travelcompass/api/plan/repository/PlanUserRepository.java
+++ src/main/java/com/travelcompass/api/plan/repository/PlanUserRepository.java
@@ -1,9 +1,9 @@
 package com.travelcompass.api.plan.repository;
 
-import com.travelcompass.api.plan.domain.PlanGroup;
+import com.travelcompass.api.plan.domain.PlanUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanGroupRepository extends JpaRepository<PlanGroup, Long> {
+public interface PlanUserRepository extends JpaRepository<PlanUser, Long> {
 }
