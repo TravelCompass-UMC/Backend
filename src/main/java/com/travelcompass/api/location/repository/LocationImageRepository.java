fef24398b5cc84652ca535edef707c86f2b8e856
enble <enble_777@naver.com>
2024-01-24 16:17:50 +0900

    Feat: LocationImageRepository에 findByLocation 메서드 추가

--- src/main/java/com/travelcompass/api/location/repository/LocationImageRepository.java
+++ src/main/java/com/travelcompass/api/location/repository/LocationImageRepository.java
@@ -1,8 +1,11 @@
 package com.travelcompass.api.location.repository;
 
+import com.travelcompass.api.location.domain.Location;
 import com.travelcompass.api.location.domain.LocationImage;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationImageRepository extends JpaRepository<LocationImage, Long> {
 
+    Optional<LocationImage> findByLocation(Location location);
 }
