c8f849fd927520274b724c38cca538c5692a9728
enble <enble_777@naver.com>
2024-01-24 15:54:11 +0900

    Feat: LocationImage 엔티티 생성

---
+++ src/main/java/com/travelcompass/api/location/domain/LocationImage.java
@@ -0,0 +1,33 @@
+package com.travelcompass.api.location.domain;
+
+import com.travelcompass.api.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+}
