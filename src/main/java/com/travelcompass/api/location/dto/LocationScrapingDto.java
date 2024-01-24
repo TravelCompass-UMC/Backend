a438e7d3b12be126dd40bda182ae460b767e1c6e
enble <enble_777@naver.com>
2024-01-24 16:38:02 +0900

    Feat: LocationScrapingDto 생성

---
+++ src/main/java/com/travelcompass/api/location/dto/LocationScrapingDto.java
@@ -0,0 +1,17 @@
+package com.travelcompass.api.location.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.checkerframework.common.aliasing.qual.NonLeaked;
+
+@Getter
+@Builder
+@NoArgsConstructor
+public class LocationScrapingDto {
+
+
+
+}
