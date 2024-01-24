1a6321fece88a86e3e0933d8e121225d826434d7
enble <enble_777@naver.com>
2024-01-24 15:54:23 +0900

    Feat: LocationImageRepository 생성

---
+++ src/main/java/com/travelcompass/api/location/repository/LocationImageRepository.java
@@ -0,0 +1,8 @@
+package com.travelcompass.api.location.repository;
+
+import com.travelcompass.api.location.domain.LocationImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationImageRepository extends JpaRepository<LocationImage, Long> {
+
+}
