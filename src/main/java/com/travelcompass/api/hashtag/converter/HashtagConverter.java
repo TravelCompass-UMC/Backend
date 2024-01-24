209dbfbcf49250cc159f8fdce25789cf98f1f26c
HyunWoo <kimjd766@gmail.com>
2024-01-24 15:11:17 +0900

    refactor: converter가 entity <-> dto 간 변환만 하도록 수정

--- src/main/java/com/travelcompass/api/hashtag/converter/HashtagConverter.java
+++ src/main/java/com/travelcompass/api/hashtag/converter/HashtagConverter.java
@@ -7,9 +7,4 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class HashtagConverter {
-
-    public static List<String> toHashtagNames(List<HashtagPlan> hashtagPlans){
-        return hashtagPlans.stream().map(HashtagPlan::getHashtag).toList()
-                .stream().map(Hashtag::getName).collect(Collectors.toList());
-    }
 }
