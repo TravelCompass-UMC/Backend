c974fc973bfdb009400ca5648f20d0da82b9c55f
HyunWoo <kimjd766@gmail.com>
2024-01-26 21:30:16 +0900

    feature: 레포지토리 기능 추가

--- src/main/java/com/travelcompass/api/plan/repository/PlanRepository.java
+++ src/main/java/com/travelcompass/api/plan/repository/PlanRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
-    Optional<Plan> findByInviteCode(String inviteCode);
+    Optional<Plan> findByInviteCode(UUID inviteCode);
 }
