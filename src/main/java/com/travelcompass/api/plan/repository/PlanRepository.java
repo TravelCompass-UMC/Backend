bca0c6678953e2c6ca03231d0af77b3c67dd65ea
HyunWoo <kimjd766@gmail.com>
2024-01-29 12:00:59 +0900

    feature: page 메서드 추가

--- src/main/java/com/travelcompass/api/plan/repository/PlanRepository.java
+++ src/main/java/com/travelcompass/api/plan/repository/PlanRepository.java
@@ -1,6 +1,8 @@
 package com.travelcompass.api.plan.repository;
 
 import com.travelcompass.api.plan.domain.Plan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.UUID;
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
     Optional<Plan> findByInviteCode(UUID inviteCode);
+
+    Page<Plan> findAll(PageRequest pageRequest);
 }
