26650d362668e4b9f35e6b5964c113e74591b454
enble <enble_777@naver.com>
2024-01-30 22:53:23 +0900

    Refactor: 리뷰 관련 로직 삭제

--- src/main/java/com/travelcompass/api/location/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travelcompass.api.location.repository;
-
-import com.travelcompass.api.location.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-}
