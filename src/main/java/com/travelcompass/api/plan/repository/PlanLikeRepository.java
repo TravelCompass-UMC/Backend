1862aa9a8fd86e6b82d49ab61a9035e3e85e7c6d
persi0815 <jina_8151010@naver.com>
2024-01-31 11:38:58 +0900

    Feature: 사용자와 계획에 대한 좋아요 정보 조회 메서드

--- src/main/java/com/travelcompass/api/plan/repository/PlanLikeRepository.java
+++ src/main/java/com/travelcompass/api/plan/repository/PlanLikeRepository.java
@@ -1,5 +1,6 @@
 package com.travelcompass.api.plan.repository;
 
+import com.travelcompass.api.oauth.domain.User;
 import com.travelcompass.api.plan.domain.Plan;
 import com.travelcompass.api.plan.domain.PlanLike;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlanLikeRepository extends JpaRepository<PlanLike, Long> {
     Long countAllByPlan(Plan plan);
+
+    // 사용자와 계획에 대한 좋아요 정보 조회
+    PlanLike findByUserAndPlan(User user, Plan plan);
 }
