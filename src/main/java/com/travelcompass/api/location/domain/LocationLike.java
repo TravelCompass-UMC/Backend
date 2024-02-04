d2a6efbfeaad61d14d14a9f6ffb05fad917cc2bd
persi0815 <jina_8151010@naver.com>
2024-02-04 11:25:11 +0900

    Feature: LocationLike 추가

---
+++ src/main/java/com/travelcompass/api/location/domain/LocationLike.java
@@ -0,0 +1,28 @@
+package com.travelcompass.api.location.domain;
+
+import com.travelcompass.api.global.entity.BaseEntity;
+import com.travelcompass.api.oauth.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationLike extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id")
+    private Location location;
+}
