37a27be6e5f002f39d607a3b307412a2c74132e8
HyunWoo <kimjd766@gmail.com>
2024-02-05 14:57:05 +0900

    feature: 내가 좋아요한 여행계획 조회 메서드 추가

--- src/main/java/com/travelcompass/api/mypage/service/MypageService.java
+++ src/main/java/com/travelcompass/api/mypage/service/MypageService.java
@@ -1,15 +1,21 @@
 package com.travelcompass.api.mypage.service;
 
+import com.travelcompass.api.global.api_payload.ApiResponse;
 import com.travelcompass.api.oauth.domain.User;
+import com.travelcompass.api.oauth.jwt.CustomUserDetails;
 import com.travelcompass.api.plan.domain.Plan;
 import com.travelcompass.api.plan.domain.PlanLike;
 import com.travelcompass.api.plan.domain.PlanUser;
+import com.travelcompass.api.plan.dto.PlanResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +27,10 @@ public class MypageService {
 
     public List<Plan> findPlansByUser(User user) {
         return user.getPlanUsers().stream().map(PlanUser::getPlan).toList();
+    }
+
+    public List<Plan> findLikedPlans(User user){
+        return user.getPlanLikes().stream().map(PlanLike::getPlan).toList();
     }
 
     public Page<Plan> convertPlanToPage(List<Plan> plans, Integer page, Integer pageSize){
