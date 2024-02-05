bb75526b562ae57f1ea5e17539a35a1027eed1f5
HyunWoo <kimjd766@gmail.com>
2024-02-05 14:17:11 +0900

    feature: 나의 여행계획 조회하는 엔드포인트 설정

--- src/main/java/com/travelcompass/api/mypage/controller/MypageController.java
+++ src/main/java/com/travelcompass/api/mypage/controller/MypageController.java
@@ -9,12 +9,21 @@ import com.travelcompass.api.mypage.service.MypageService;
 import com.travelcompass.api.oauth.domain.User;
 import com.travelcompass.api.oauth.jwt.CustomUserDetails;
 import com.travelcompass.api.oauth.service.UserService;
+import com.travelcompass.api.plan.converter.PlanConverter;
+import com.travelcompass.api.plan.domain.Plan;
+import com.travelcompass.api.plan.dto.PlanResponseDto;
+import com.travelcompass.api.plan.dto.PlanResponseDto.PlanListResponseDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.travelcompass.api.plan.dto.PlanResponseDto.*;
 
 @Tag(name = "마이페이지", description = "마이페이지 관련 api 입니다.")
 @RequiredArgsConstructor
@@ -25,6 +34,7 @@ public class MypageController {
     private final MypageService mypageService;
     private final UserService userService;
 
+    // 나의 정보 조회
     @GetMapping("/info")
     public ApiResponse<MyInfoDto> findMyInfo(
             @AuthenticationPrincipal CustomUserDetails customUserDetails
@@ -32,5 +42,18 @@ public class MypageController {
         User user = userService.findUserByUserName(customUserDetails.getUsername());
         return ApiResponse.onSuccess(SuccessCode.MYPAGE_INFO_VIEW_SUCCESS, MypageConverter.myInfoDto(user));
     }
+
+    // 나의 계획 조회
+    @GetMapping("/plans")
+    public ApiResponse<List<SimplePlanDto>> findMyPlans(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        User user = userService.findUserByUserName(customUserDetails.getUsername());
+        List<Plan> plans = mypageService.findUserPlans(user);
+        List<SimplePlanDto> planDtos = plans.stream().map(PlanConverter::simplePlanDto).toList();
+
+        return ApiResponse.onSuccess(SuccessCode.MYPAGE_PLAN_LIST_VIEW_SUCCESS, planDtos);
+    }
+
 
 }
