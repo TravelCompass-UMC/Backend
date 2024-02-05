eb69c2f938c273c465591c04ed7b361385849623
HyunWoo <kimjd766@gmail.com>
2024-02-05 21:33:20 +0900

    feature: 추가한 DTO들에 대한 컨버터 추가

--- src/main/java/com/travelcompass/api/mypage/converter/MypageConverter.java
+++ src/main/java/com/travelcompass/api/mypage/converter/MypageConverter.java
@@ -1,8 +1,18 @@
 package com.travelcompass.api.mypage.converter;
 
+import com.travelcompass.api.address.domain.Address;
+import com.travelcompass.api.location.domain.Location;
 import com.travelcompass.api.mypage.dto.MypageResponseDto;
 import com.travelcompass.api.mypage.dto.MypageResponseDto.MyInfoDto;
+import com.travelcompass.api.mypage.dto.MypageResponseDto.MyLocationListResponseDto;
+import com.travelcompass.api.mypage.dto.MypageResponseDto.MypageLocationDto;
 import com.travelcompass.api.oauth.domain.User;
+import com.travelcompass.api.plan.converter.PlanConverter;
+import com.travelcompass.api.plan.domain.Plan;
+import com.travelcompass.api.plan.dto.PlanResponseDto;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public class MypageConverter {
 
@@ -12,6 +22,29 @@ public class MypageConverter {
                 .nickname(user.getNickname())
                 .email(user.getEmail())
                 .password(user.getPassword())
+                .build();
+    }
+
+    public static MypageLocationDto mypageLocationDto(Location location){
+        return MypageLocationDto.builder()
+                .id(location.getId())
+                .name(location.getName())
+                .locationType(String.valueOf(location.getLocationType()))
+                .likeCount(location.getLikeCount())
+                .address(location.getAddress().toString())
+                .build();
+    }
+
+    public static MyLocationListResponseDto myLocationListResponseDto(Page<Location> locations){
+        List<MypageLocationDto> locationDtos = locations.stream().map(MypageConverter::mypageLocationDto).toList();
+
+        return MyLocationListResponseDto.builder()
+                .isLast(locations.isLast())
+                .isFirst(locations.isFirst())
+                .totalPage(locations.getTotalPages())
+                .totalElements(locations.getTotalElements())
+                .listSize(locations.getSize())
+                .locationList(locationDtos)
                 .build();
     }
 
