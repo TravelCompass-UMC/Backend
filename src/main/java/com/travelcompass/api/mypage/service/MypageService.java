47c252f912f6cfc90965a018f5d9a3b4b6868338
HyunWoo <kimjd766@gmail.com>
2024-02-05 14:46:35 +0900

    refactor: 여행계획 조회 메서드가 페이징된 리스트 반환하게 수정

--- src/main/java/com/travelcompass/api/mypage/service/MypageService.java
+++ src/main/java/com/travelcompass/api/mypage/service/MypageService.java
@@ -2,8 +2,12 @@ package com.travelcompass.api.mypage.service;
 
 import com.travelcompass.api.oauth.domain.User;
 import com.travelcompass.api.plan.domain.Plan;
+import com.travelcompass.api.plan.domain.PlanLike;
 import com.travelcompass.api.plan.domain.PlanUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +19,16 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class MypageService {
 
-    public List<Plan> findUserPlans(User user) {
-        return user.getPlanUsers().stream().map(PlanUser::getPlan).collect(Collectors.toList());
+    public Page<Plan> findPlansByUser(User user, Integer page, Integer pageSize) {
+        List<Plan> plans = user.getPlanUsers().stream().map(PlanUser::getPlan).toList();
+
+        int start = page * pageSize;
+        int end = Math.min(start + pageSize, plans.size());
+        List<Plan> pagedPlans = plans.subList(start, end);
+
+        return new PageImpl<>(pagedPlans, PageRequest.of(page, pageSize), plans.size());
+    }
+
+
     }
 }
