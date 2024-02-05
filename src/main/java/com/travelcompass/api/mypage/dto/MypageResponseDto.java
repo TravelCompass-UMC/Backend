126d0419699eb07e839366bba046c2f3bbf2897d
HyunWoo <kimjd766@gmail.com>
2024-02-05 21:32:58 +0900

    feature: location을 리스트로 반환하기 위한 DTO 추가

--- src/main/java/com/travelcompass/api/mypage/dto/MypageResponseDto.java
+++ src/main/java/com/travelcompass/api/mypage/dto/MypageResponseDto.java
@@ -1,12 +1,8 @@
 package com.travelcompass.api.mypage.dto;
 
-import com.travelcompass.api.location.domain.LocationLike;
-import com.travelcompass.api.plan.domain.PlanLike;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MypageResponseDto {
@@ -21,5 +17,53 @@ public class MypageResponseDto {
         private String password;
         private String email;
         private String nickname;
+    }
+
+    @Schema(description = "MypageLocationDto")
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MypageLocationDto{
+        @Schema(description = "장소 id")
+        private Long id;
+
+        @Schema(description = "장소명")
+        private String name;
+
+        @Schema(description = "장소 타입")
+        private String locationType;
+
+        @Schema(description = "장소 주소")
+        private String address;
+
+        @Schema(description = "장소 좋아요 수")
+        private Long likeCount;
+    }
+
+
+    @Schema(description = "MyLocationListResponseDto")
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyLocationListResponseDto{
+        @Schema(description = "마지막 페이지가 맞으면 참, 아니면 거짓")
+        private Boolean isLast;
+
+        @Schema(description = "첫번째 페이지가 맞으면 참, 아니면 거짓")
+        private Boolean isFirst;
+
+        @Schema(description = "총 페이지 수")
+        private Integer totalPage;
+
+        @Schema(description = "총 location 수")
+        private Long totalElements;
+
+        @Schema(description = "locationList의 크기")
+        private Integer listSize;
+
+        @Schema(description = "여행계획 리스트, 한 번 요청에 12개씩 전달함")
+        private List<MypageLocationDto> locationList;
     }
 }
