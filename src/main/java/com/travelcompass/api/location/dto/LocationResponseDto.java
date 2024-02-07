1c71c8aa8b6781fb7641190021a287ea3eaa515d
enble <enble_777@naver.com>
2024-02-07 22:29:52 +0900

    Feat: LocationResponseDto 구현

--- src/main/java/com/travelcompass/api/location/dto/LocationResponseDto.java
+++ src/main/java/com/travelcompass/api/location/dto/LocationResponseDto.java
@@ -1,6 +1,7 @@
 package com.travelcompass.api.location.dto;
 
 import com.travelcompass.api.location.domain.LocationType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,26 +17,37 @@ public class LocationResponseDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class DetailLocationResponseDto {
+    public static class DetailLocationDto {
 
+        @Schema(description = "장소 id")
         private Long id;
 
+        @Schema(description = "장소명")
         private String name;
 
+        @Schema(description = "장소 타입", example = "ATTRACTION", allowableValues = {"ATTRACTION",
+                "RESTAURANT", "CAFE", "ACCOMMODATION"})
         private LocationType locationType;
 
+        @Schema(description = "별점")
         private Double star;
 
+        @Schema(description = "도로명 주소")
         private String roadNameAddress;
 
+        @Schema(description = "영업시간 리스트")
         private List<BusinessHoursDto> businessHoursDtos;
 
+        @Schema(description = "전화번호")
         private String tel;
 
+        @Schema(description = "이미지 URL")
         private String imageUrl;
 
+        @Schema(description = "위도")
         private Double latitude;
 
+        @Schema(description = "경도")
         private Double longitude;
     }
 
