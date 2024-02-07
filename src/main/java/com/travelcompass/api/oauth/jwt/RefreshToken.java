42aeaca81e12c0492f6fb4feeb9591be613d49a7
persi0815 <jina_8151010@naver.com>
2024-02-07 21:15:01 +0900

    Feature: refresh token entity

--- src/main/java/com/travelcompass/api/oauth/jwt/RefreshToken.java
+++ src/main/java/com/travelcompass/api/oauth/jwt/RefreshToken.java
@@ -1,40 +1,37 @@
-//package com.travelcompass.api.oauth.jwt;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.redis.core.RedisHash;
-//import org.springframework.data.redis.core.TimeToLive;
-//import org.springframework.data.redis.core.index.Indexed;
-//import org.springframework.security.core.GrantedAuthority;
-//
-//import java.util.Collection;
-//@Builder
-//@Getter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@RedisHash(value = "refresh",  timeToLive = 1000L)
-//public class RefreshToken {
-//
-//    @Id
-//    private String id;
-//
-//    private String ip;
-//
+package com.travelcompass.api.oauth.jwt;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+    @Id
+    private String id;
+
+    private String ip;
+
+    @ElementCollection
+    private Collection<String> authorities; // 권한을 문자열 형태로 저장
 //    private Collection<? extends GrantedAuthority> authorities;
-//
-//    @Indexed
-//    private String refreshToken;
-//
-//    @TimeToLive
-//    private Long ttl;
-//
-//    public void updateRefreshToken(String refreshToken) {
-//        this.refreshToken = refreshToken;
-//    }
-//    public void updateTtl(Long ttl) {
-//        this.ttl = ttl;
-//    }
-//}
+
+    private String refreshToken;
+
+    private Long ttl;
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public void updateTtl(Long ttl) {
+        this.ttl = ttl;
+    }
+}
