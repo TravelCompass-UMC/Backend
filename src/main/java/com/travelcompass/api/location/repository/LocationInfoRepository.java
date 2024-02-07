ccd53df4b53eee84119a30e46be3d02704c461ef
enble <enble_777@naver.com>
2024-02-07 19:38:11 +0900

    Refactor: findByLocationId 메서드 추가

--- src/main/java/com/travelcompass/api/location/repository/LocationInfoRepository.java
+++ src/main/java/com/travelcompass/api/location/repository/LocationInfoRepository.java
@@ -1,8 +1,9 @@
 package com.travelcompass.api.location.repository;
 
 import com.travelcompass.api.location.domain.LocationInfo;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationInfoRepository extends JpaRepository<LocationInfo, Long> {
-
+    Optional<LocationInfo> findByLocationId(Long locationId);
 }
