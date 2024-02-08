dc4875b7504c4af00fb8a7c275292c5b3827c75d
HyunWoo <kimjd766@gmail.com>
2024-02-08 16:11:50 +0900

    refactor: locationInfo와 1대1 매칭 추가

--- src/main/java/com/travelcompass/api/location/domain/Location.java
+++ src/main/java/com/travelcompass/api/location/domain/Location.java
@@ -2,13 +2,7 @@ package com.travelcompass.api.location.domain;
 
 import com.travelcompass.api.global.entity.BaseEntity;
 import com.travelcompass.api.region.domain.Region;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -47,6 +41,10 @@ public class Location extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "region_id")
     private Region region;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_info_id")
+    private LocationInfo locationInfo;
 
 //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "ADDRESS_CODE")
