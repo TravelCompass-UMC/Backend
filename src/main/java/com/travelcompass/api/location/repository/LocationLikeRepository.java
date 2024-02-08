e297cd21103261980c9bdcd54ec2e39f5e22589b
enble <enble_777@naver.com>
2024-02-08 21:16:43 +0900

    Refactor: findAllByUserAndRegion 메서드 추가

--- src/main/java/com/travelcompass/api/location/repository/LocationLikeRepository.java
+++ src/main/java/com/travelcompass/api/location/repository/LocationLikeRepository.java
@@ -3,8 +3,10 @@ package com.travelcompass.api.location.repository;
 import com.travelcompass.api.location.domain.Location;
 import com.travelcompass.api.location.domain.LocationLike;
 import com.travelcompass.api.oauth.domain.User;
+import com.travelcompass.api.region.domain.Region;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,5 +17,7 @@ public interface LocationLikeRepository extends JpaRepository<LocationLike, Long
     LocationLike findByUserAndLocation(User user, Location location);
 
     // 특정 사용자의 좋아요한 LocationLike 목록 조회
-    List<LocationLike> findAllByUser(User user);
+    // RegionId도 인자로 받아 해당 지역의 장소만 조회할 수 있도록 구현
+    @Query("select ll from LocationLike ll join ll.location l where ll.user = :user and l.region = :region")
+    List<LocationLike> findAllByUserAndRegion(User user, Region region);
 }
