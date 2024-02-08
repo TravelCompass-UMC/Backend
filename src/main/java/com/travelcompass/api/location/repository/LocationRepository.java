115249a01e28776e4f1f6bd0747952602c582903
enble <enble_777@naver.com>
2024-02-08 16:38:38 +0900

    Refactor: 반환형 수정

--- src/main/java/com/travelcompass/api/location/repository/LocationRepository.java
+++ src/main/java/com/travelcompass/api/location/repository/LocationRepository.java
@@ -1,6 +1,7 @@
 package com.travelcompass.api.location.repository;
 
 import com.travelcompass.api.location.domain.Location;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,5 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
 
     Optional<Location> findByName(String name);
 
-    Optional<Location> findByRegionId(Long regionId);
+    List<Location> findAllByRegionId(Long regionId);
 }
