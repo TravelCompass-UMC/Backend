56e82f8f46bd2491d94c09b21f22bba771f3f0b4
enble <enble_777@naver.com>
2024-02-09 17:45:34 +0900

    Style: 클래스 구조 변경

--- src/main/java/com/travelcompass/api/address/service/Coordinates.java
+++ src/main/java/com/travelcompass/api/address/service/Coordinates.java
@@ -1,6 +1,5 @@
 package com.travelcompass.api.address.service;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +7,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Coordinates {
-    private String x; // longitude
-    private String y; // latitude
+    private String longitude;
+    private String latitude;
+
+    public Coordinates(String longitude, String latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
 }
