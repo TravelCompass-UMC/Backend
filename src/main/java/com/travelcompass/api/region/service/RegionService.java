539c5461bff313e6bc3b12d303b7d6b94439f3c9
enble <enble_777@naver.com>
2024-02-12 18:36:39 +0900

    Feat: findAllRegion 메서드 추가

--- src/main/java/com/travelcompass/api/region/service/RegionService.java
+++ src/main/java/com/travelcompass/api/region/service/RegionService.java
@@ -4,6 +4,7 @@ import com.travelcompass.api.global.api_payload.ErrorCode;
 import com.travelcompass.api.global.exception.GeneralException;
 import com.travelcompass.api.region.domain.Region;
 import com.travelcompass.api.region.repository.RegionRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +16,16 @@ public class RegionService {
 
     private final RegionRepository repository;
 
+    public List<Region> findAllRegions() {
+        return repository.findAll();
+    }
+
     public Region findRegionById(Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> GeneralException.of(ErrorCode.REGION_NOT_FOUND));
     }
 
-    public Region findRegionByName(String name){
+    public Region findRegionByName(String name) {
         return repository.findByName(name)
                 .orElseThrow(() -> GeneralException.of(ErrorCode.REGION_NOT_FOUND));
     }
