b27b0176cc2d81f3571243c7a6ea8501daaaeacb
enble <enble_777@naver.com>
2024-02-12 18:39:30 +0900

    Feat: swagger 추가

--- src/main/java/com/travelcompass/api/region/controller/RegionController.java
+++ src/main/java/com/travelcompass/api/region/controller/RegionController.java
@@ -4,6 +4,9 @@ import com.travelcompass.api.global.api_payload.ApiResponse;
 import com.travelcompass.api.region.converter.RegionConverter;
 import com.travelcompass.api.region.dto.RegionResponseDto.DetailRegionDto;
 import com.travelcompass.api.region.service.RegionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,11 @@ public class RegionController {
 
     private final RegionService regionService;
 
+    @Operation(summary = "지역 목록 조회", description = "지역 목록을 조회하는 메서드입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON_200", description = "Success"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REGION_404", description = "지역을 찾을 수 없습니다.")
+    })
     @GetMapping()
     public ApiResponse<List<DetailRegionDto>> getRegionList() {
         List<DetailRegionDto> findList = regionService.findAllRegions().stream()
@@ -29,11 +37,23 @@ public class RegionController {
         return ApiResponse.onSuccess(findList);
     }
 
+    @Operation(summary = "지역 상세 조회", description = "지역 상세 정보를 조회하는 메서드입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON_200", description = "Success"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REGION_404", description = "지역을 찾을 수 없습니다.")
+    })
+    @Parameter(name = "regionId", description = "지역 id", required = true)
     @GetMapping("/{regionId}")
     public ApiResponse<DetailRegionDto> getRegionDetail(@PathVariable Long regionId) {
         return ApiResponse.onSuccess(RegionConverter.toRegionDto(regionService.findRegionById(regionId)));
     }
 
+    @Operation(summary = "지역 이름으로 조회", description = "지역 이름으로 조회하는 메서드입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON_200", description = "Success"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "REGION_404", description = "지역을 찾을 수 없습니다.")
+    })
+    @Parameter(name = "name", description = "지역 이름", required = true)
     @GetMapping("/search")
     public ApiResponse<DetailRegionDto> getRegionByName(@RequestParam(name = "name") String name) {
         return ApiResponse.onSuccess(RegionConverter.toRegionDto(regionService.findRegionByName(name)));
