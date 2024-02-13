05c176b22e4c246b45354eec5ec57d5001653de9
HyunWoo <kimjd766@gmail.com>
2024-02-13 18:32:10 +0900

    feature: JpaSpecificationExecutor extend 추가

--- src/main/java/com/travelcompass/api/plan/repository/PlanRepository.java
+++ src/main/java/com/travelcompass/api/plan/repository/PlanRepository.java
@@ -5,14 +5,16 @@ import com.travelcompass.api.region.domain.Region;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface PlanRepository extends JpaRepository<Plan, Long> {
+public interface PlanRepository extends JpaRepository<Plan, Long>, JpaSpecificationExecutor<Plan> {
     Optional<Plan> findByInviteCode(UUID inviteCode);
 
-    Page<Plan> findAllByRegion(Region region, PageRequest pageRequest);
+    List<Plan> findAllByRegion(Region region);
 }
